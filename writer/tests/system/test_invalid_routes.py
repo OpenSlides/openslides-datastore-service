9ec8ce5d3d6e452854bcb34d9a12d2fe7f872abd
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2020-03-18 15:29:49 +0100

    test

--- writer/tests/system/test_invalid_routes.py
+++ writer/tests/system/test_invalid_routes.py
@@ -12,7 +12,7 @@ def test_wrong_method_get_ids(client):
 
 
 def test_404_on_unknown_url_1(client):
-    response = client.get("/")
+    response = client.get("/") #test
     assert response.status_code == 404
     response = client.post("/", data={})
     assert response.status_code == 404
