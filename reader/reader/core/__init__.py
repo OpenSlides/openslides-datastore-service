c2b98591e0b2e4e6911b48f6616560f3cfb460d0
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2020-07-07 12:11:40 +0200

    Update isort command and and add explicit pip update make command

--- reader/reader/core/__init__.py
+++ reader/reader/core/__init__.py
@@ -3,6 +3,7 @@ from .reader import Reader
 
 def setup_di():
     from shared.di import injector
+
     from .reader_service import ReaderService
 
     injector.register(Reader, ReaderService)
--- shared/shared/postgresql_backend/__init__.py
+++ shared/shared/postgresql_backend/__init__.py
@@ -4,10 +4,11 @@ from .sql_event_types import EVENT_TYPES  # noqa
 
 def setup_di():
     from shared.di import injector
-    from .sql_read_database_backend_service import SqlReadDatabaseBackendService
-    from shared.services import ReadDatabase
-    from .pg_connection_handler import PgConnectionHandlerService
     from shared.postgresql_backend.sql_query_helper import SqlQueryHelper
+    from shared.services import ReadDatabase
+
+    from .pg_connection_handler import PgConnectionHandlerService
+    from .sql_read_database_backend_service import SqlReadDatabaseBackendService
 
     injector.register(ConnectionHandler, PgConnectionHandlerService)
     injector.register(SqlQueryHelper, SqlQueryHelper)
