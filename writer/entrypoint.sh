18124bcfdc12586e2e39372f78cf8a2e3e9bdfe5
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2020-10-12 19:45:47 +0200

    Move schema creation to writer

--- writer/entrypoint.sh
+++ writer/entrypoint.sh
@@ -3,6 +3,11 @@
 source wait-for-message-bus.sh
 source wait-for-database.sh
 
+# Create schema in postgresql
+export PGPASSWORD="$DATASTORE_DATABASE_PASSWORD"
+psql -1 -h "$DATASTORE_DATABASE_HOST" -p "$DATASTORE_DATABASE_PORT" -U "$DATASTORE_DATABASE_USER" \
+        -d "$DATASTORE_DATABASE_NAME" -f shared/postgresql_backend/schema.sql
+
 if [ -n "$COMMAND" ]; then
     if [ -f "cli/$COMMAND.py" ]; then
         echo "executing $COMMAND"
