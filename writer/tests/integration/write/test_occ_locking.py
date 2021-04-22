40bb5739c24cc77bde703fe6d4cc4851e4b903cf
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2021-04-22 15:27:35 +0200

    Remove unneeded arrays

--- writer/tests/integration/write/test_occ_locking.py
+++ writer/tests/integration/write/test_occ_locking.py
@@ -106,12 +106,10 @@ def test_locked_collectionfield_with_filter(
 ):
     locked_collectionfield = MagicMock()
     connection_handler.collectionfield = MagicMock(return_value=locked_collectionfield)
-    valid_metadata["locked_fields"]["a/f"] = [
-        {
-            "position": 42,
-            "filter": FilterOperator("field", "=", "value"),
-        }
-    ]
+    valid_metadata["locked_fields"]["a/f"] = {
+        "position": 42,
+        "filter": FilterOperator("field", "=", "value"),
+    }
 
     with pytest.raises(ModelLocked) as e:
         write_handler.write(valid_metadata)
@@ -124,7 +122,7 @@ def test_locked_collectionfield_with_filter_without_filter(
 ):
     locked_collectionfield = MagicMock()
     connection_handler.collectionfield = MagicMock(return_value=locked_collectionfield)
-    valid_metadata["locked_fields"]["a/f"] = [{"position": 42}]
+    valid_metadata["locked_fields"]["a/f"] = {"position": 42}
 
     with pytest.raises(ModelLocked) as e:
         write_handler.write(valid_metadata)
