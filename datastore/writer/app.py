0dc43063df8c939ed23a66280debf872b9143424
Oskar Hahn <mail@oshahn.de>
2022-01-07 14:29:54 +0100

    draft: add opentelemetry

--- datastore/writer/app.py
+++ datastore/writer/app.py
@@ -9,3 +9,30 @@ def create_application():
 
 
 application = create_application()
+
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+
+
+span_exporter = OTLPSpanExporter(
+    # optional
+    endpoint="collector:4317",
+    # credentials=ChannelCredentials(credentials),
+    # headers=(("metadata", "metadata")),
+)
+tracer_provider = TracerProvider(
+    resource=Resource.create({SERVICE_NAME: "datastore-writer"})
+)
+trace.set_tracer_provider(tracer_provider)
+span_processor = BatchSpanProcessor(span_exporter)
+tracer_provider.add_span_processor(span_processor)
+
+FlaskInstrumentor().instrument_app(application)
