8ce0855ebbcc0b7baa2cd0a6b6dc7e38fefae30c
peb-adr <adrichter97@gmail.com>
2022-09-15 11:47:41 +0200

    add read-from-stdin to cli/import-events.sh for better compatibility (#208)
    * add read-from-stdin to cli/import-events.sh
    
    * use wait-for-database.sh
    
    Co-authored-by: Adrian Richter <adrian@intevation.de>

--- cli/import-events.sh
+++ cli/import-events.sh
@@ -3,8 +3,15 @@
 set -e
 
 if [ $# -eq 0 ]; then
-    echo "No file to import from is given."
-    exit 1
+  echo "No file to import from is given (use - to read from stdin)."
+  exit 1
 fi
 
-psql -h "$DATASTORE_DATABASE_HOST" -p "$DATASTORE_DATABASE_PORT" -U "$DATASTORE_DATABASE_USER" -d "$DATASTORE_DATABASE_NAME" < $1
+SRC=
+[ "$1" = "-" ] ||
+  SRC="$1"
+
+# wait for potentially restarted postgres to become ready
+source wait-for-database.sh
+
+cat $SRC | psql -h "$DATASTORE_DATABASE_HOST" -p "$DATASTORE_DATABASE_PORT" -U "$DATASTORE_DATABASE_USER" -d "$DATASTORE_DATABASE_NAME"
