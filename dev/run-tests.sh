48960c0c8b9cec73641fedbe13094011387bfe05
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-23 15:06:23 +0200

    Lint scripts via Shellcheck

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -12,7 +12,7 @@ PERSIST_CONTAINERS=$1
 # Setup
 IMAGE_TAG=openslides-datastore-tests
 CATCH=0
-CHOWN="$(id -u ${USER}):$(id -g ${USER})"
+CHOWN="$(id -u "${USER}"):$(id -g "${USER}")"
 
 # Execution
 if [ "$(docker images -q $IMAGE_TAG)" = "" ]; then make build-test || CATCH=1; fi
@@ -20,6 +20,6 @@ docker compose -f dc.test.yml up -d || CATCH=1
 docker compose -f dc.test.yml exec -T datastore bash -c "chown -R $CHOWN /app" || CATCH=1
 docker compose -f dc.test.yml exec datastore ./entrypoint.sh pytest || CATCH=1
 
-if [ -z $PERSIST_CONTAINERS ]; then docker compose -f dc.test.yml down || CATCH=1; fi
+if [ -z "$PERSIST_CONTAINERS" ]; then docker compose -f dc.test.yml down || CATCH=1; fi
 
 exit $CATCH
