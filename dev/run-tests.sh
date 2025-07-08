ed216f7603a18a20a49197161d21f2e71d5d63e0
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-07-08 15:22:17 +0200

    Adding trap to all tests

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -6,20 +6,18 @@ echo "########################################################################"
 echo "###################### Run Tests and Linters ###########################"
 echo "########################################################################"
 
-# Parameters
-PERSIST_CONTAINERS=$1
-
 # Setup
 IMAGE_TAG=openslides-datastore-tests
 CATCH=0
 CHOWN="$(id -u "${USER}"):$(id -g "${USER}")"
+
+# Safe Exit
+trap 'docker compose -f dc.test.yml down' EXIT
 
 # Execution
 if [ "$(docker images -q $IMAGE_TAG)" = "" ]; then make build-test || CATCH=1; fi
 docker compose -f dc.test.yml up -d || CATCH=1
 docker compose -f dc.test.yml exec -T datastore bash -c "chown -R $CHOWN /app" || CATCH=1
 docker compose -f dc.test.yml exec datastore ./entrypoint.sh pytest || CATCH=1
-
-if [ -z "$PERSIST_CONTAINERS" ]; then docker compose -f dc.test.yml down || CATCH=1; fi
 
 exit $CATCH
